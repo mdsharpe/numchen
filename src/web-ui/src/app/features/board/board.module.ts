e7020dc9234fe81e2b3240108acb2d65cf019dcc
Matthew Sharpe <mds@gn.apc.org>
2018-10-18 20:58:40 +0100

    Fix failing build

--- src/web-ui/src/app/features/board/board.module.ts
+++ src/web-ui/src/app/features/board/board.module.ts
@@ -9,6 +9,7 @@ import { ColumnComponent } from './column/column.component';
 import { SourceStackComponent } from './source-stack/source-stack.component';
 import { GoalStackComponent } from './goal-stack/goal-stack.component';
 import { CardComponent } from './card/card.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   imports: [
@@ -21,7 +22,8 @@ import { CardComponent } from './card/card.component';
     ColumnComponent,
     SourceStackComponent,
     GoalStackComponent,
-    CardComponent
+    CardComponent,
+    ScoreComponent
   ],
   exports: [BoardComponent]
 })
