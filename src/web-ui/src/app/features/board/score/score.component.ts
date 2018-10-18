31b5a8f448105ee237e14e4c47602388a0e325da
Matthew Sharpe <mds@gn.apc.org>
2018-10-18 20:56:05 +0100

    Add score component

--- src/web-ui/src/app/features/board/score/score.component.ts
+++ src/web-ui/src/app/features/board/score/score.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-    selector: 'app-goal-stack',
-    templateUrl: './goal-stack.component.html',
-    styleUrls: ['./goal-stack.component.scss'],
+    selector: 'app-score',
+    templateUrl: './score.component.html',
+    styleUrls: ['./score.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScoreComponent {
